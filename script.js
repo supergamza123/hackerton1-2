7469c0785ef7206b5e332a5f64e50c66925dd2e3
supergamza123 <lisheahun123@gmail.com>
2024-01-10 12:59:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const messageInput = document.getElementById('userInput');
-
-//     messageInput.addEventListener('keypress', (e) => {
-//         if (e.key === 'Enter') {
-//             e.preventDefault(); // 기본 Enter 행동을 막습니다.
-//             sendMessage();
-//         }
-//     });
-// });
-
-// function sendMessage() {
-//     const messageInput = document.getElementById('userInput');
-//     const chatContainer = document.getElementById('chatContainer'); // 변경된 부분
-
-//     const message = messageInput.value.trim();
-//     if (message !== '') {
-//         const messageElement = document.createElement('div');
-//         messageElement.className = 'message user-message';
-//         messageElement.textContent = message;
-
-//         chatContainer.appendChild(messageElement);
-//         messageInput.value = '';
-//     }
-// }
-
 document.addEventListener('DOMContentLoaded', () => {
     const initialMessage = "초기 메뉴 메시지입니다.";
     document.getElementById('menuContent').textContent = initialMessage;
